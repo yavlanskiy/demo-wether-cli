ab957c63564ad5f72c7746038fc7b45713898205
Maksim Yavlyanskiy <max_yav@csltd.com.ua>
2023-08-19 22:30:02 +0300

    add file system

--- weather.js
+++ weather.js
@@ -1,11 +1,29 @@
 #!usr/bin/env node
 import {getArgs} from './helpers/args.js'
-import {printHelp} from "./services/log.service.js";
+import {printError, printHelp, printSuccess} from "./services/log.service.js";
+import {saveKeyValue} from "./services/storage.service.js";
 
+
+const saveToken = async (token) => {
+    try {
+        await saveKeyValue('token', token)
+        printSuccess('Токен сохранен!')
+    } catch (e){
+        printError(e)
+    }
+
+
+}
 const initCLI = () => {
     const args = getArgs(process.argv)
     if (args.h) {
         printHelp()
+    }
+    if (args.s) {
+
+    }
+    if (args.t) {
+        return saveToken(args.t)
     }
 }
 
