2fff70d3eba132888d4471c7e4770b11f5ca482f
Maksim Yavlyanskiy <max_yav@csltd.com.ua>
2023-08-20 13:26:37 +0300

    printError('Token не может быть пустым.')

--- weather.js
+++ weather.js
@@ -5,6 +5,10 @@ import {saveKeyValue} from "./services/storage.service.js";
 
 
 const saveToken = async (token) => {
+    if (!token.length) {
+        printError('Token не может быть пустым.')
+        return
+    }
     try {
         await saveKeyValue('token', token)
         printSuccess('Токен сохранен!')
